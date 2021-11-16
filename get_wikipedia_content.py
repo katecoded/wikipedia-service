99e39dbaae4ddbf0f93a711ee2a96440390d9719
Katelyn Lindsey <katelyn.lindsey77@gmail.com>
2021-11-16 02:15:00 -0800

    Changes response text content from one long string to string array.

--- get_wikipedia_content.py
+++ get_wikipedia_content.py
@@ -22,12 +22,10 @@ def get_relevant_wiki_page(search_term):
 	query = {"action": "query", "format": "json", "list": "search", "srsearch": search_term, "srlimit": 1}
 
 	# request the data
-	# print("Requesting Page Data:", file=stderr)
 	response = requests.get(url=URL, params=query)
 	response = response.json()
-	# print("Done requesting page data", file=stderr)
 
-	# get relevant data in order to get page content later
+	# get relevant data of most relevant page in order to get page content later
 	page_data = {"title": response["query"]["search"][0]["title"], "pageid": response["query"]["search"][0]["pageid"]}
 
 	return page_data
@@ -35,49 +33,49 @@ def get_relevant_wiki_page(search_term):
 
 def get_html(page_data):
 	"""
-	Returns the html text from the wikipedia page as a dictionary in HTML format.
+	Returns the HTML text from the wikipedia page as a dictionary in HTML format.
 	"""
 
 	# first, format the query to get just the page HTML content
 	query = {"action": "parse", "pageid": page_data["pageid"], "format": "json", "prop": "text"}
 
 	# request the page HTML content
-	# print("Requesting Page HTML:", file=stderr)
 	response = requests.get(url=URL, params=query)
 	response = response.json()
-	# print("Done requesting page HTML", file=stderr)
 
 	return response["parse"]["text"]["*"]
 
 
 def strip_html(page_content):
 	"""
-	Returns a string of text after stripping HTML fron the
+	Returns an array of paragraphs after stripping HTML fron the
 	page content using BeautifulSoup. 
 	"""
 
 	# prepare the html to be parsed
 	parsed_content = BeautifulSoup(page_content, "html.parser")
 
-	# place where all text will be stored
-	complete_text = ""
-
 	# get all of the paragraphs in the content
 	paragraphs = parsed_content.find_all("p")
 
-	# store all of the paragraphs as one large string
+	# create list to store complete text in
+	complete_text = []
+
+	# store all of the paragraphs in a list
 	for paragraph in paragraphs:
 
-		complete_text += paragraph.get_text() + " "
+		text = paragraph.get_text()
+
+		if text != "" and text != "\n":
+			complete_text.append(text)
 
 	# return the completed text
-	# print("Page text is as follows\n", complete_text, file=stderr)
 	return complete_text
 
 
 def remove_brackets(text):
 	"""
-	Takes the complete text of a wikipedia page stripped of HTML and
+	Takes a string of text from a wikipedia page stripped of HTML and
 	returns a new string of that text with extraneous brackets removed.
 	"""
 
@@ -106,10 +104,10 @@ def remove_brackets(text):
 	return cleaned_text
 
 
-def clean_special_characters(text):
+def fix_special_characters(text):
 	"""
-	Takes the complete content of a wikipedia page without brackets or html,
-	and returns that text with special characters cleaned up to display normally.
+	Takes a string of text from a wikipedia page without brackets or HTML,
+	and returns that text with special characters fixed to display normally.
 	"""
 
 	# clean up the text
@@ -119,28 +117,43 @@ def clean_special_characters(text):
 	return cleaned_text	
 
 
+def clean_text(text_array):
+	"""
+	Takes an array of strings and cleans it up by removing brackets
+	and ensuring special characters display properly.
+	"""
+
+	# loop through the array of strings, cleaning up each string one by one
+	for index in range(len(text_array)):
+
+		# remove extraneous brackets
+		text_array[index] = remove_brackets(text_array[index])
+
+		# clean up special characters
+		text_array[index] = fix_special_characters(text_array[index])
+
+
 def get_wikipedia_content(search_term):
 	"""
 	Uses the Wikipedia API to first get pages associated
-	with search term. Then, get specific page html content.
+	with search term. Then, get specific page HTML content,
+	get the paragraphs from that content, and format that
+	as JSON to return.
 	"""
 
 	# get the data of the most relevant page
 	page_data = get_relevant_wiki_page(search_term)
 
-	# get the HTML content of that page
+	# get all of the HTML content of that page
 	page_content = get_html(page_data)
 
-	# get just text content
-	text = strip_html(page_content)
+	# strip the HTML to get an array of paragraph strings
+	paragraph_array = strip_html(page_content)
 
-	# remove extraneous brackets from text content
-	text = remove_brackets(text)
-
-	# clean up special characters
-	text = clean_special_characters(text)
+	# clean up the array of strings (remove brackets and fix special characters)
+	clean_text(paragraph_array)
 
 	# format the JSON that will be returned as the response
-	response_content = {"title": page_data["title"], "text_content": text}
+	response_content = {"title": page_data["title"], "text_content": paragraph_array}
 
 	return response_content
